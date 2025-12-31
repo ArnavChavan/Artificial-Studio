dfdf75bb9638a225910fdd523510d88cddb14e5a
ArnavChavan <arnavchavan360@gmail.com>
2025-12-31 10:37:45 +0530

    Add files via upload

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,1 +1,9 @@
-
+export default function Login() {
+  return (
+    <div className="login">
+      <h1>Artificial Hub Studio</h1>
+      <p>Sign in to continue</p>
+      <button className="float-btn">Continue with Google</button>
+    </div>
+  );
+}
